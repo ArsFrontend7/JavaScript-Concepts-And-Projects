5a36fe3e1c36eb26f00c7819852494d4a7a97871
Arifur Rahman <ars.ict2021@gmail.com>
2024-08-05 10:35:30 +0600

    Refreshed

---
+++ 01. Basics/1. Variables and Constants.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const cSharp = ['ASP.NET MVC', 'ASP.NET Web API', 'ASP.NET Core', 'Blazor', 'MAUI'];
+const php = ['Laravel', 'Codeigniter', 'Symfony', 'CakePHP', 'Yii'];
+const java = ['Spring Boot', 'JavaServer Faces', 'Struts', 'Play', 'Grails'];
+const python = ['Django', 'Flask', 'Pyramid', 'FastAPI', 'Tornado'];
+const javaScript = ['Angular', 'React', 'Vue', 'Express', 'Meteor'];
+
+console.table([cSharp, php, java, python, javaScript]);
